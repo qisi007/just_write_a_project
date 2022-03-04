01614c33e321808a1537808ddaecb23ce637a18a
liuguisheng <codeliuguisheng@163.com>
2022-03-04 16:13:01 +0800

    feat(java): 添加swagger

--- just_java/src/main/java/com/example/just_java/controller/table.java
+++ just_java/src/main/java/com/example/just_java/controller/table.java
@@ -3,7 +3,9 @@ package com.example.just_java.controller;
 
 import com.example.just_java.common.response.ResponseDTO;
 import com.example.just_java.service.TableService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +16,8 @@ public class table {
     @Autowired
     private TableService tableService;
 
-    @RequestMapping("list")
+    @ApiOperation(value = "获取表格数据", notes = "")
+    @GetMapping("list")
     public ResponseDTO getTable () {
         return tableService.getTableDate();
     }
